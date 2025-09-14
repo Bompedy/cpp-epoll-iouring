56b2c58ab59546bafd307d64f8b9878cd041e755
Lucas <lucasphone75@gmail.com>
2025-09-13 18:50:21 -0600

    update scripts

--- client.sh
+++ client.sh
@@ -7,8 +7,8 @@ export IS_CLIENT=1
 export LEADER_ADDRESS="10.10.1.1:7069"
 export HOST_ADDRESS="10.10.1.4:8000"
 export CONNECTIONS=10
-export OPS=100000
-export DATA_SIZE=10000
+export OPS=1000000
+export DATA_SIZE=1000
 
 echo "Launching client..."
 ./build/epolluringtest
--- node2.sh
+++ node2.sh
@@ -7,8 +7,8 @@ echo "Setting up environment for NODE 0..."
 export IS_CLIENT=0
 export NODE_ID=2
 export LEADER_ID=0
-export BUFFER_SIZE=11000
-export LOG_SIZE=150000
+export BUFFER_SIZE=1100
+export LOG_SIZE=1001000
 export CLIENT_LISTENER="10.10.1.3:7069"
 export PEERS="10.10.1.1:6969,10.10.1.2:6969,10.10.1.3:6969"
 
