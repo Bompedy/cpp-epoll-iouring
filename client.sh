a780cc1f2c0ef185cf922950538190ea9750ab68
Lucas <lucasphone75@gmail.com>
2025-09-13 18:08:31 -0600

    run script

---
+++ client.sh
@@ -0,0 +1,14 @@
+
+set -e
+
+echo "Setting up environment for CLIENT..."
+
+export IS_CLIENT=1
+export LEADER_ADDRESS="10.10.1.1:7069"
+export HOST_ADDRESS="10.10.1.4:8000"
+export CONNECTIONS=10
+export OPS=100000
+export DATA_SIZE=10000
+
+echo "Launching client..."
+./build/epolluringtest
