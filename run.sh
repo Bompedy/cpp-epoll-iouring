3b470cb2790679150914fd800bf4ec6d907d4a0a
Lucas <lucasphone75@gmail.com>
2025-09-13 18:04:14 -0600

    run script

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+echo "Setting up environment for NODE 0..."
+
+export IS_CLIENT=0
+export NODE_ID=0
+export LEADER_ID=0
+export BUFFER_SIZE=11000
+export LOG_SIZE=150000
+export CLIENT_LISTENER="127.0.0.1:7069"
+export PEERS="127.0.0.1:6969,127.0.0.1:6970,127.0.0.1:6971"
+
+./build/epolluringtest
